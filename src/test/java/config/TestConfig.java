2e1125471c0fef806334e56f9f7e5d01cf9eb2dc
Ivan Goropashny <goropashny@gmail.com>
2025-08-25 21:33:49 +0400

    Login tests, config and utils

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -1,4 +1,18 @@
 package config;
 
+import com.codeborne.selenide.Configuration;
+
 public class TestConfig {
+    public static void setup() {
+        Configuration.browser = System.getProperty("browser", "chrome"); // Значение по умолчанию
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.timeout = Long.parseLong(System.getProperty("timeout", "5000"));
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        Configuration.baseUrl = System.getProperty("baseUrl", "http://194.87.222.160");
+
+        // Дополнительные настройки
+        Configuration.screenshots = true;
+        Configuration.savePageSource = false;
+    }
 }
